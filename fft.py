8086b83aafe292ace2faf5c5bc825576dc05176c
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2023-11-22 20:27:42 -0500

    convert image into numpy array

--- fft.py
+++ fft.py
@@ -1,6 +1,8 @@
 import argparse # for parsing command line arguments
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import cv2 as cv
 
 class Fft:
     parser = argparse.ArgumentParser()
@@ -9,29 +11,59 @@ class Fft:
 
     args = parser.parse_args()
 
-    def fast_mode(self):
-        print("fast mode")
+    img = ""
 
-    def denoising(self):
-        print("denoising")
+    # Mode 1
+    def mode_1(self):
+        print("mode 1")
+        # simply perform the FFT and output a one by two subplot of the original image and next to it its Fourier transform. 
 
-    def compressing(self):
-        print("compressing")
+        # The Fourier transform should be log scaled. 
+        # An easy way to do this with matplotlib is to import LogNorm from matplotlib.colors to produce a logarithmic colormap.
 
-    def plotting(self):
-        print("plotting")
+    # Mode 2
+    def mode_2(self):
+        print("mode 2")
+
+    # Mode 3
+    def mode_3(self):
+        print("mode 3")
+
+    # Mode 4
+    def mode_4(self):
+        print("mode 4")
+
+    def convert_image_into_array(self):
+        img = cv.imread(self.args.i, cv.IMREAD_GRAYSCALE)
+
+        # If the image given doesn't have a length or width that is a power of 2 
+        # resize it with cv2 otherwise the FFT algorithm might not work
+        
+        # get current width and height
+        h, w = img.shape[:2]
+        if (w & (w-1)) != 0  | (h & (h-1)) !=0 : 
+            w = int(2 ** np.ceil(np.log2(w)))
+            h = int(2 ** np.ceil(np.log2(h)))
+            img = cv.resize(img, (w,h))
+
+        # cv.imshow("Display window", img)
+        # K = cv.waitKey(0) # Wait for a keystroke in the window
+
 
 if __name__ == "__main__":
     # program starts running here
     fft = Fft()
 
+    # convert image into a NumPy array
+    fft.convert_image_into_array()
+
     if(fft.args.m == 1):
-        fft.fast_mode()
+        fft.mode_1()
     elif(fft.args.m == 2):
-        fft.denoising()
+        fft.mode_2()
     elif(fft.args.m == 3):
-        fft.compressing()
+        fft.mode_3()
     elif(fft.args.m == 4):
-        fft.plotting()
+        fft.mode_4()
 
 
